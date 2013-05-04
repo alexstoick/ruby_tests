17b8bb07b20c4e6905a351c13e2b3c2543507afb
Alex Stoica <alex_stoicajr@yahoo.co.uk>
2013-05-04 19:31:01 +0300

    slight changes to seeds

---
+++ parser.rb
@@ -0,0 +1,56 @@
+# encoding: UTF-8
+require 'nokogiri'
+require 'open-uri'
+require 'json'
+
+page = Nokogiri::HTML(open('http://www.cinemagia.ro/program-cinema/bucuresti/'))
+
+json = []
+
+puts page.css(".program_cinema_show").length
+
+page.css(".program_cinema_show").each do |film|
+
+	titluRo = film.css(".title_ro").text
+	titluEn = film.css("h2:first").text
+	details = film.css(".info")[0]
+
+	nota = details.css("div")[0].text
+	gen = details.css("div")[1].text
+	actori = details.css("div")[2].text
+	regizor = details.css("div")[3].text
+
+	gen = gen.gsub( /\s\s+/ , '' )
+	actori = actori.gsub( /\s\s+/ , '' )
+	regizor = regizor.gsub( /\s\s+/ , '' )
+
+	cinematografe = film.css(".mb5")
+
+	cinematografe.each do |cinematograf|
+		cinemaName = cinematograf.css(".theatre-link")[0].text
+		length = cinematograf.css("div").length
+		if length > 0
+
+			program = cinematograf.css("div")[0].text
+
+			program = program.gsub(/[^0-9:]/i, '')
+			length = (program.length)/5-1
+			for i in 0..length
+				ora = program[i*5 , 5 ]
+				intrare = {}
+				intrare [ "titluEn" ] = titluEn
+				intrare [ "titluRo" ] = titluRo
+				intrare [ "cinema" ] = cinemaName
+				intrare [ "ora" ] = ora ;
+				intrare [ "nota" ] = nota ;
+				intrare [ "gen" ] = gen ;
+				intrare [ "actori" ] = actori ;
+				intrare [ "regizor" ] = regizor ;
+				json.push ( intrare )
+			end
+		end
+	end
+end
+
+File.open( 'date.json', 'w' , encoding: 'ISO-8859-1' ) { |file| file.write( JSON.generate(json) ) }
+
