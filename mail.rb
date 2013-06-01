bd6318bc0c50530776c961e409a050178be70a37
Alex Stoica <alex_stoicajr@yahoo.co.uk>
2013-06-01 11:33:48 +0300

    updated weather & mail

--- mail.rb
+++ mail.rb
@@ -9,6 +9,6 @@ EOF
 
 smtp = Net::SMTP.new 'smtp.gmail.com', 587
 smtp.enable_starttls
-smtp.start('gmail.com', 'alex.stoicajr@gmail.com', '#######', :login)
+smtp.start('gmail.com', 'alex.stoicajr@gmail.com', "##" , :login)
 smtp.send_message message, 'FROM@gmail.com', 'alex.stoicajr@gmail.com'
 smtp.finish
--- weather.rb
+++ weather.rb
@@ -13,7 +13,7 @@ loop {
 
 	info = page.css ( ".wx-first" )
 
-	condition = info.css(".wx-phrase").text
+	condition = "Condition - " + info.css(".wx-phrase").text
 
 	temp = info.css(".wx-temp").text
 	temp_c = ((temp.to_i-32)/1.8).round(2)
@@ -22,32 +22,39 @@ loop {
 
 	feels_like = details.css("dl")[0].text.gsub( /\n/ , '' )
 	humidity = details.css("dl")[1].text.gsub( /\n/ , '' )
-	precip = details.css("dl")[2].text.gsub( /\n/ , '' )
+	precip = details.css("dl")[2].text.gsub( /\n/ , '' ).to_i
 	wind = details.css("dl")[3].text.gsub( /\n/ , '' )
 
 	time = Time.now
 	time.localtime ( "+03:00")
 	time = time.to_s
 
-	message = "Subject: Weather status\n"
+	message = "Subject: Weather status at #{time} \n"
 
-	message +=  "CONDITION: " + condition + "\nTEMPERATURE: " + temp_c.to_s	+ 
-				"\nPRECIPATION: " + (precip.to_i).to_s + "%\n" + wind + "\n" + time
+	# message += condition + "; \t" + temp_c.to_s + "\n" +
+	# 		humidity + "\n" + "Precipitation:" + precip.to_s + "\n" + wind
+	# # message = "Subject: Weather status "+ "\n"
 
-	if ( condition != old_condition || precip != old_precip || temp_c != old_temp_c )
+	txt = 'abc '
+
+	message +=  condition + "\n" + "TEMPERATURE: " + temp_c.to_s	+
+				"\n" + "PRECIPATION: " + (precip.to_i).to_s + "%" + "\n" + wind + "\n" + time
+
+	if ( condition != old_condition || precip != old_precip )
 		old_condition = condition
 		old_precip = precip
 		old_temp_c = temp_c
 
-		password = 'oowqzqkdsahfbmub'
-		puts message
+		password = '####'
 		smtp = Net::SMTP.new 'smtp.gmail.com', 587
 		smtp.enable_starttls
 		smtp.start('gmail.com', 'alex.stoicajr@gmail.com', password , :login)
 		smtp.send_message message, 'FROM@gmail.com', 'alex.stoicajr@gmail.com'
 		smtp.finish
-
 		puts "Sent at " + time
+	else
+		puts 'same conditions'
 	end
+
 	sleep 60*60
 }
