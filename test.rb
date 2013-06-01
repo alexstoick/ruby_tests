704deeb918abacc180bd08c0987801c8ead694dd
Alex Stoica <alex_stoicajr@yahoo.co.uk>
2013-06-01 11:01:51 +0300

    working on weather sender

--- test.rb
+++ test.rb
@@ -1,6 +1,4 @@
-require 'open-uri'
 time = Time.now
-string = "*&%^Boys Like Girls - Hero/Heroine"
-string1 = "abc"
-printf( "%-40s after time: %5.2f\n" , string , (Time.now - time) )
-printf( "%-40s after time: %5d\n" , string1 , (Time.now - time).to_i )
+time.localtime ( "+03:00")
+
+puts time
---
+++ weather.rb
@@ -0,0 +1,53 @@
+require 'nokogiri'
+require 'open-uri'
+require 'net/smtp'
+
+link = 'http://www.weather.com/weather/hourbyhour/graph/ROXX0003'
+
+old_condition = ""
+old_temp_c = 0
+old_precip = 0
+
+loop {
+	page = Nokogiri::HTML( open( link ) )
+
+	info = page.css ( ".wx-first" )
+
+	condition = info.css(".wx-phrase").text
+
+	temp = info.css(".wx-temp").text
+	temp_c = ((temp.to_i-32)/1.8).round(2)
+
+	details = info.css(".wx-details")
+
+	feels_like = details.css("dl")[0].text.gsub( /\n/ , '' )
+	humidity = details.css("dl")[1].text.gsub( /\n/ , '' )
+	precip = details.css("dl")[2].text.gsub( /\n/ , '' )
+	wind = details.css("dl")[3].text.gsub( /\n/ , '' )
+
+	time = Time.now
+	time.localtime ( "+03:00")
+	time = time.to_s
+
+	message = "Subject: Weather status\n"
+
+	message +=  "CONDITION: " + condition + "\nTEMPERATURE: " + temp_c.to_s	+ 
+				"\nPRECIPATION: " + (precip.to_i).to_s + "%\n" + wind + "\n" + time
+
+	if ( condition != old_condition || precip != old_precip || temp_c != old_temp_c )
+		old_condition = condition
+		old_precip = precip
+		old_temp_c = temp_c
+
+		password = 'oowqzqkdsahfbmub'
+		puts message
+		smtp = Net::SMTP.new 'smtp.gmail.com', 587
+		smtp.enable_starttls
+		smtp.start('gmail.com', 'alex.stoicajr@gmail.com', password , :login)
+		smtp.send_message message, 'FROM@gmail.com', 'alex.stoicajr@gmail.com'
+		smtp.finish
+
+		puts "Sent at " + time
+	end
+	sleep 60*60
+}
